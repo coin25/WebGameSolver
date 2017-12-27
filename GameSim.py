c2387f5c7e6d3369acd70efc133c8b83814742c3
Austin Young <azzy6105@gmail.com>
2017-12-27 11:15:48 -0500

    Adding basic FindRange class

--- GameSim.py
+++ GameSim.py
@@ -4,11 +4,31 @@ from time import sleep
 class ClickImage:
     def click(self, image, delay = 1):
         sleep(delay)
-        image_x, image_y = pyautogui.center(pyautogui.locateOnScreen("Images/" + image))
+        image_x, image_y = pyautogui.locateCenterOnScreen("Images/" + image)
         pyautogui.click(image_x, image_y)
+
+class FindRange:
+    possibleDigits = [str(i) + ".PNG" for i in range(1,10)]
+    digitLocations = []
+
+    def __init__(self):
+        self.region = pyautogui.screenshot(region=())
+        for i in self.possibleDigits:
+            #Easier for organization and later sorting
+            print(i)
+            try:
+                image_x, image_y = pyautogui.locateCenterOnScreen("Images/" + i)
+                self.digitLocations.append((image_x, image_y))
+            except:
+                pass
+        print(self.digitLocations)
 
 
 class Bot:
     def start(self, seconds = 2):
         sleep(seconds)
+        test = FindRange()
 
+bot = Bot()
+bot.start(5)
+pyautogui.lo
